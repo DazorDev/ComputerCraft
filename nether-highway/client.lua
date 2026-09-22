d5b97f7d16f230dda05e4730366ca6778e488d45
Daniel Banic <95126947+DazorDev@users.noreply.github.com>
2026-09-22 18:28:49 +0200

    feat: add lua files

---
+++ nether-highway/client.lua
@@ -0,0 +1,82 @@
+local server = nil
+local left_edge_3f = nil
+local right_edge_3f = nil
+local function turtle_block_3f()
+  local res, block = turtle.inspect()
+  return (res and ((block.name == "computercraft:turtle_normal") or (block.name == "computercraft:turtle_advanced")))
+end
+local function edge_turtle_3f()
+  turtle.turnLeft()
+  if not turtle_block_3f() then
+    left_edge_3f = true
+  else
+  end
+  turtle.turnRight()
+  turtle.turnRight()
+  if not turtle_block_3f() then
+    right_edge_3f = true
+    return nil
+  else
+    return nil
+  end
+end
+local function init()
+  local configured_3f = nil
+  while not configured_3f do
+    peripheral.find("modem", rednet.open)
+    do
+      local id, msg = rednet.receive("nether-highway")
+      local setup_3f = (msg == "setup")
+      if setup_3f then
+        edge_turtle_3f()
+        configured_3f = true
+      else
+      end
+    end
+    rednet.send(server, "configured", "nether-highway")
+  end
+  return nil
+end
+local function finished_step()
+  return rednet.send(server, "done", "nether-highway")
+end
+local function step()
+  turtle.dig()
+  turtle.digUp()
+  if left_edge_3f then
+    turtle.turnLeft()
+    turtle.place()
+    turtle.turnRight()
+  else
+  end
+  if right_edge_3f then
+    turtle.turnRight()
+    turtle.place()
+    turtle.turnLeft()
+  else
+  end
+  turtle.forward()
+  return finished_step()
+end
+local function main()
+  local running_3f = true
+  while running_3f do
+    local id, msg = rednet.receive("nether-highway")
+    local server_3f = (id == server)
+    if server_3f then
+      if (msg == "done") then
+        running_3f = nil
+      elseif (msg == "continue") then
+        step()
+      else
+      end
+    else
+    end
+  end
+  return nil
+end
+if ... then
+  return main()
+else
+  return nil
+end
---
+++ nether-highway/server.lua
@@ -0,0 +1,62 @@
+local configured_3f = nil
+local clients = 0
+local clients_done = 0
+local function main()
+  init()
+  return update()
+end
+local function init()
+  peripheral.find("modem", rednet.open)
+  rednet.broadcast("setup", "nether-highway")
+  return parallel.waitForAny(__fnl_global__handle_2dtimer, __fnl_global__handle_2dregistering)
+end
+local function handle_timer()
+  local timer = os.startTimer(1)
+  __fnl_global__wait_2dfor_2dtimer(timer)
+  configured_3f = true
+  return nil
+end
+local function wait_for_timer(timer_id)
+  local timer_done_3f = nil
+  while not timer_done_3f do
+    local _, id = os.pullEvent("timer")
+    if (id == timer_id) then
+      __fnl_global__timer_2ddone = true
+    else
+    end
+  end
+  return nil
+end
+local function handle_registering()
+  while not configured_3f do
+    local id, message = rednet.receive()
+    local register_turtle_3f = (message == "configured")
+    if register_turtle_3f then
+      clients = (clients + 1)
+    else
+    end
+  end
+  return nil
+end
+local function update()
+  local running_3f = true
+  while running_3f do
+    local id, message = rednet.receive("nether-highway")
+    local is_client_3f = clients[id]
+    if is_client_3f then
+      clients_done = (clients + done + 1)
+      if (clients == clients_done) then
+        clients_done = 0
+        rednet.broadcast("continue", "nether-highway")
+      else
+      end
+    else
+    end
+  end
+  return nil
+end
+if ... then
+  return main()
+else
+  return nil
+end
